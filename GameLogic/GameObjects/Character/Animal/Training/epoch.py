1f496e1acccd984e87e9bdc5466a0bddbd11da8b
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2024-01-26 20:33:00 -0800

    Epoch description

--- GameLogic/GameObjects/Character/Animal/Training/epoch.py
+++ GameLogic/GameObjects/Character/Animal/Training/epoch.py
@@ -1,5 +1,9 @@
 
 class Epoch:
+    """
+    Epoch object (Epoch is period of time, in this case a generation that is made to end and reproduce)
+    """
+
     def __init__(self):
         self.count = 0
         self.top_selection_amount = 15
