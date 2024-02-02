586b38ce9257cbfa267c765d301fef89bab3d0a1
TrevorBuchanan <123215971+TrevorBuchanan@users.noreply.github.com>
2024-02-02 09:09:30 -0800

    Refactoring game running from main to game class

--- GameLogic/Game/game.py
+++ GameLogic/Game/game.py
@@ -1,8 +1,9 @@
 import pygame
 
-from GameLogic.GameObjects.Character.Player.player_manager import PlayerManager
 from GameLogic.GameUtilities import colors
-from GameLogic.GameUtilities.settings import SCREEN, INPUT
+from GameLogic.GameUtilities.colors import WHITE
+from GameLogic.GameUtilities.settings import SCREEN, INPUT, BORDER, FPS
+from GameLogic.GameUtilities.utility import write_to_screen
 
 
 class Game:
@@ -10,7 +11,41 @@ class Game:
         self.game_objects = []
         self.game_clock = 0
 
-    def run_game_iteration(self):
+    def run_game(self):
+        """
+        Runs Game
+        """
+        clock = pygame.time.Clock()
+        # Screen window loop
+        running = True
+        while running:
+            # Set the frame rates
+            clock.tick(FPS)
+            # Check for closure
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+                    quit()
+            # Fill the background
+            SCREEN.fill(colors.DARK_GREEN)
+            # Get user inputs
+            INPUT.keys = pygame.key.get_pressed()
+            # Run a game iteration
+            self.run_iteration()
+            # *** Temp *** Show FPS to screen
+            fps = str(int(clock.get_fps()))
+            write_to_screen(f"FPS: {fps}", [BORDER.width - 120, 10], WHITE)
+            # Display to screen
+            pygame.display.update()
+        # End pygame
+        pygame.quit()
+
+    def run_iteration(self):
+        """
+        Performs one iteration of the game
+        """
         # Fill the background
         SCREEN.fill(colors.DARK_GREEN)
 
--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 # By Trevor Buchanan
 
-import pygame
-
 from GameLogic.GameObjects.Character.Animal.animal_manager import AnimalManager
 from GameLogic.GameObjects.Character.Player.player_manager import PlayerManager
 from GameLogic.GameObjects.Enviroment.plant_manager import PlantManager
 from GameLogic.Game.game import Game
 from GameLogic.GameObjects.Character.Animal.Training.epoch_manager import EpochManager
-from GameLogic.GameUtilities.colors import WHITE
-from GameLogic.GameUtilities.settings import FPS, BORDER, ANIMAL_AMOUNT, PLANT_AMOUNT
-from GameLogic.GameUtilities.utility import write_to_screen
+from GameLogic.GameUtilities.settings import ANIMAL_AMOUNT, PLANT_AMOUNT
 
 if __name__ == '__main__':
-    clock = pygame.time.Clock()
-
     # Set up the game
     g = Game()
     for _ in range(ANIMAL_AMOUNT):
@@ -25,25 +19,4 @@ if __name__ == '__main__':
     g.game_objects.append(EpochManager())
     g.game_objects.append(PlayerManager())
 
-    # Screen window loop
-    running = True
-    while running:
-        # Set the frame rates
-        clock.tick(FPS)
-
-        # Check for closure
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                quit()
-
-        g.run_game_iteration()
-
-        fps = str(int(clock.get_fps()))
-        write_to_screen(f"FPS: {fps}", [BORDER.width - 120, 10], WHITE)
-
-        pygame.display.update()
-
-pygame.quit()
+    g.run_game()
